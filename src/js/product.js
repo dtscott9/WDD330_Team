713d314934984f82a4b7aa5c06ccfbd2efa8b034
DarthZ96 <mcc17067@byui.edu>
2022-09-28 14:42:53 -0600

    Team Activity 2.4

--- src/js/product.js
+++ src/js/product.js
@@ -1,32 +1,15 @@
+import ProductData from './productData.js';
+const dataSource = new ProductData('tents');
+console.log(dataSource.getData());
+
 let products = [];
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
 
 function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
 
-// get tents data
-function getProductsData() {
-  fetch("../json/tents.json")
-    .then(convertToJson)
-    .then((data) => {
-      products = data;
-    });
-}
-// or should we do it this way?
-// async function getProductsDataAwait() {
-//   products = await fetch("../json/tents.json").then(convertToJson);
-// }
-
 // add to cart button event handler
 function addToCart(e) {
-  const product = products.find((item) => item.Id === e.target.dataset.id);
   setLocalStorage("so-cart", product);
 }
 
