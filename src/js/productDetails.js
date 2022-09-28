9a069bab63a130ba13902f871b17895828d42180
dtscott9 <dtscott9@byui.edu>
2022-09-28 14:53:37 -0600

    wokring on productDetails.js

---
+++ src/js/productDetails.js
@@ -0,0 +1,19 @@
+export default class productDetails {
+    constructor(productId, dataSource){
+        this.productId = productId;
+        this.product = {};
+        this.dataSource = dataSource;
+    }
+
+    init() {
+
+    }
+
+    addToCart() {
+
+    }
+
+    renderProductDetails() {
+        
+    }
+}
