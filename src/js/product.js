904918f071e3496d484ebf083ea60fdb7a423cd8
dtscott9 <dtscott9@byui.edu>
2022-09-30 15:11:06 -0600

    product render method complete

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,9 @@
 import ProductData from './productData.js';
 import productDetails from './productDetails.js';
-import { getParams } from './utils.js';
+import { getParam } from './utils.js';
 
 const dataSource = new ProductData('tents');
-const productId = getParams('product');
+const productId = getParam('product');
 
 const product = new productDetails(productId, dataSource);
 product.init();
--- src/js/productDetails.js
+++ src/js/productDetails.js
@@ -1,4 +1,5 @@
-import productData from "./productData.js";
+import { setLocalStorage } from "./utils.js";
+
 
 export default class productDetails {
   constructor(productId, dataSource) {
@@ -14,18 +15,49 @@ export default class productDetails {
     // once we have the product details we can render out the HTML
     // once the HTML is rendered we can add a listener to Add to Cart button
     // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-    this.product = productData.findProductById.then(this.dataSource)
-    document.querySelector("main").textContent = this.product
+    this.product = await this.dataSource.findProductById(this.productId);
+    document.querySelector("main").innerHTML = this.renderProductDetails();
     document
       .getElementById("addToCart")
       .addEventListener("click", this.addToCart.bind(this));
   }
 
-  addToCart(e) {
+  addToCart() {
     setLocalStorage("so-cart", products);
   }
 
   renderProductDetails() {
+    return `<section class="product-detail">
+    <h3 class="product_brand">${this.product.Brand.Name}
+    </h3>
+    <h2 class="divider" class="product_name">
+      ${this.product.NameWithoutBrand}
+      Ajax Tent - 3-Person, 3-Season</h2>
+    <img
+      class="divider"
+      src="${this.product.Image}"
+      alt="${this.product.NameWithoutBrand}"
+    />
+    <p class="product-card__price"><s>$${this.product.SuggestedRetailPrice}
+    
+    </s></p>
+    <p class="product-discount-price">$${this.product.ListPrice}
+      <span id="discount">Save: $${Math.round(
+        this.product.SuggestedRetailPrice - this.product.ListPrice)
+      }</span></p>
 
+    <p class="product__color">
+      ${this.product.Colors[0].ColorName}
+    </p>
+    <p class="product__description">
+
+      ${this.product.DescriptionHtmlSimple}
+
+    </p>
+    <div class="product-detail__add">
+      
+      <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+    </div>
+  </section>`;
   }
 }
