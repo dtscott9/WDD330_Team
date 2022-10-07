2af3020689adc4292e6ea02590b857632704bc93
DarthZ96 <mcc17067@byui.edu>
2022-10-07 15:16:09 -0600

    correct

--- src/js/productList.js
+++ src/js/productList.js
@@ -26,11 +26,10 @@ export default class ProductList {
     }
 
     renderList(list) {
+        // make sure the list is empty
+        this.listElement.innerHTML = '';
+        //get the template
         const template = document.getElementById('product-card-template');
-        list.forEach(product => {
-        const clone = template.content.cloneNode(true);
-        const hydratedTemplate = this.prepareTemplate(clone, product);
-        this.listElement.appendChild(hydratedTemplate);
-        })
+        renderListWithTemplate(template, this.listElement, list, this.prepareTemplate);
     } 
 }
