2749e4e663741019c8be93a7c96365ddae1b0328
DarthZ96 <mcc17067@byui.edu>
2022-10-14 14:04:17 -0600

    yes

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
 import ProductData from "./productData";
 import ProductList from "./productList";
+import { loadHeaderFooter } from './utils.js';
 
-const list = document.querySelector(".product-list")
+loadHeaderFooter();
+
 const productData = new ProductData("tents");
+const list = document.querySelector(".product-list")
 const productList = new ProductList("tents", productData, list);
 
+productList.init();
