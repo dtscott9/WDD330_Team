9ccc489eeacfb50279b3a8377a7345d079a498b7
Seth Wilding <sawilding7@gmail.com>
2022-10-21 14:37:16 -0600

    remove from cart button

--- src/js/cartList.js
+++ src/js/cartList.js
@@ -1,4 +1,4 @@
-import { renderListWithTemplate, getLocalStorage } from './utils.js';
+import { renderListWithTemplate, getLocalStorage, setLocalStorage } from './utils.js';
 import productDetails from './productDetails.js';
 
 export default class CartList {
@@ -14,15 +14,31 @@ export default class CartList {
   }
   
   prepareTemplate(template, product) {
-    
+    console.log(product);
     template.querySelector('.cart-card__image img').src =  product.Images.PrimarySmall;
+    // template.querySelector(".cart-card__image img").src = product.Image;
     template.querySelector('.cart-card__image img').alt += product.Name;
     template.querySelector('.card__name').textContent = product.Name;
-    template.querySelector('.cart-card__color').textContent = product.Colors[0].ColorName;
+    // template.querySelector('.cart-card__color').textContent = product.Colors[0].ColorName;
     template.querySelector('.cart-card__price').textContent += product.FinalPrice; 
+    template.querySelector(".cart-card__remove").addEventListener("click", () => {
+      const itemId = product.Id;
+      console.log(itemId);
+      const cart = getLocalStorage("so-cart")
+      for (let i=0; i < cart.length; i++) {
+        if (cart[i].Id == itemId) {
+          console.log("hello")
+          cart.splice(i, 1);
+          console.log(cart);
+          setLocalStorage("so-cart", cart)
+          location.reload();
+        }
+      }
+    })
     return template;
   }
-  
+
+
   renderList(list) {
     // make sure the list is empty
     this.listElement.innerHTML = '';
